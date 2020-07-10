7aeca930bbe3353aed232d89dd02ad6232a99c9c
Hannah <HannahCheyenne@gmail.com>
2020-07-10 16:06:06 -0500

    jquery additions

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 /**
- * Example store structure
+ * 
+ * Technical requirements:
+ * 
+ * Your app should include a render() function, that regenerates the view each time the store is updated. 
+ * See your course material, consult your instructor, and reference the slides for more details.
+ *
+ * NO additional HTML elements should be added to the index.html file.
+ *
+ * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
+ *
+ * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
+ * 
  */
+
 const store = {
   // 5 or more questions are required
   questions: [
@@ -60,29 +72,157 @@ const store = {
   score: 0
 };
 
-/**
- * 
- * Technical requirements:
- * 
- * Your app should include a render() function, that regenerates the view each time the store is updated. 
- * See your course material, consult your instructor, and reference the slides for more details.
- *
- * NO additional HTML elements should be added to the index.html file.
- *
- * You may add attributes (classes, ids, etc) to the existing HTML elements, or link stylesheets or additional scripts if necessary
- *
- * SEE BELOW FOR THE CATEGORIES OF THE TYPES OF FUNCTIONS YOU WILL BE CREATING 👇
- * 
- */
 
 /********** TEMPLATE GENERATION FUNCTIONS **********/
 
 // These functions return HTML templates
 
+function startQuizTemplate () {
+  return `<header>
+  <div class="box"> 
+      <div class="imgwrap">
+          <img src="/images/SOT-logo.png" alt="Sea of Thieves Logo"/> 
+      </div>
+  </div>
+  </header>
+  <main class="main">
+    <div class="title">
+        <h1>Sea of Thieves Trivia</h1>
+    </div>
+    <div class="thing">
+        <p>This is a fun quiz game using the lore from the videogame Sea of Thieves, test yerrr knowledge!</p>
+        <button type="button">Start Quiz</button>
+    </div>
+  </main>`;
+}
+
+function questionTemplate(questionNumber, score) {
+  return `<header class="item">
+  <h1>Sea of Thieves Trivia</h1>
+  </header>
+  <main>
+    <div class="container">
+        <div class="score">
+          <p>${questionNumber} out of 5</p>
+          <p>${score} correct, ${score-questionNumber} incorrect</p>
+        </div>
+      <div class="item">
+        <div class="js-question">
+          <h1>${store.questions[questionNumber].question}</h1>
+        </div>
+        <form id="js-answer">
+          <label><input type='radio' name='answer' value='${store.questions[questionNumber].answers[0]}'>${store.questions[questionNumber].answers[0]}</label><br>
+          <label><input type='radio' name='answer' value='${store.questions[questionNumber].answers[1]}'>${store.questions[questionNumber].answers[1]}</label><br>
+          <label><input type='radio' name='answer' value='${store.questions[questionNumber].answers[2]}'>${store.questions[questionNumber].answers[2]}</label><br>
+          <label><input type='radio' name='answer' value='${store.questions[questionNumber].answers[3]}'>${store.questions[questionNumber].answers[3]}</label><br>
+          <button type="submit">Check Question</button>
+        </form>
+      </div>
+    </div>
+  </main>`;
+}
+
+function feedbackTemplate(questionNumber, isCorrect) {
+  if(isCorrect) {
+    store.score++;
+  }
+  return `<header class="item">
+    <div class="feedback">
+        <h1>${isCorrect ? 'Correct Answer!' : 'The correct answer is: '+ store.questions[questionNumber].correctAnswer}</h1>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+        <h2>- Score -</h2>
+        <div class="score-feedback">
+            <p>Right: ${store.score} </p>
+            <p>Wrong: ${store.questionNumber-store.score}</p>
+        </div>
+        <div class="js-feedback-button">
+            <button type="button">Next Question</button>
+        </div>
+    </div>
+  </main>`;
+}
+
+function finalScoreTemplate(){
+  return `<header class="item">
+    <div class="restart">
+        <h1>Your Results</h1>
+    </div>
+  </header>
+  <main>
+    <div class="container">
+        <h2>- Score -</h2>
+        <div class="score-final">
+            <p>Right: ${store.score}</p>
+            <p>Wrong: ${store.questionNumber-store.score}</p>
+        </div>
+        <div class="js-restart-button">
+            <button type="button">Restart Quiz</button>
+        </div>
+    </div>
+  </main>`;
+}
+
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+function render(template) {
+  $('body').html(template);
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+function eventHandler() {
+  handleNewGame();
+  handleSubmitAnswer();
+  handleNextQuestion();
+  handleRestart();
+}
+
+function handleNewGame() {
+  $('body').on('click', '.js-restart-button', () => {
+    render(startQuizTemplate(store.questionNumber));
+    store.quizStarted = true;
+  });
+}
+
+function handleSubmitAnswer() {
+  $('body').on('click', '#js-answer', event => {
+    event.preventDefault();
+    let userAnswer= $(event.target).attr('value');
+    render(feedbackTemplate(store.questionNumber, userAnswer === store.questions[store.questionNumber].correctAnswer));
+  }); 
+}
+
+function handleNextQuestion() {
+  $('body').on('click','.js-feedback-button', () => {
+    store.questionNumber++;
+    if(store.questionNumber === store.questions.length) {
+      render(finalScoreTemplate());
+    } else {
+      render(questionTemplate(store.questionNumber));
+    }
+  });
+}
+
+function handleRestart() {
+  $('body').on('click','.js-restart-button', () => {
+    store.questionNumber = 0;
+    store.score = 0;
+    store.quizStarted = 0;
+    render(startQuizTemplate());
+  });
+}
+
+
+function executeQuizApp() { 
+  if(!store.quizStarted) {
+    render(startQuizTemplate());
+  }
+  eventHandler();
+}
+
+$(executeQuizApp);
